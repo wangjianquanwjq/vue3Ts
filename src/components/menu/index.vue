df0a38c2e70bfebd2858dcc7f5cdfd5db08a71ea
wangjianquanwjq <610743994@qq.com>
2022-04-28 17:32:06 +0800

    1

--- src/components/menu/index.vue
+++ src/components/menu/index.vue
@@ -1,12 +1,6 @@
 <template>
-  <el-menu
-    active-text-color="#ffd04b"
-    background-color="#545c64"
-    class="el-menu-vertical-demo"
-    default-active="1-1"
-    text-color="#fff"
-    :collapse="isCollapse"
-  >
+  <el-menu active-text-color="#ffd04b" background-color="#545c64" class="el-menu-vertical-demo" default-active="1-1"
+    text-color="#fff" :collapse="isCollapse">
     <el-sub-menu :index="item.value" v-for="(item) in menuData" :key="item.value">
       <template #title>
         <el-icon>
@@ -15,12 +9,8 @@
         <span>{{ item.label }}</span>
       </template>
       <el-menu-item-group>
-        <el-menu-item
-          :index="itemData.value"
-          v-for="(itemData) in item.children"
-          :key="itemData.value"
-          @click="goMenu(itemData)"
-        >{{ itemData.label }}</el-menu-item>
+        <el-menu-item :index="itemData.value" v-for="(itemData) in item.children" :key="itemData.value"
+          @click="goMenu(itemData)">{{ itemData.label }}</el-menu-item>
       </el-menu-item-group>
     </el-sub-menu>
   </el-menu>
@@ -33,9 +23,34 @@ import {
   Menu as IconMenu,
   Setting
 } from "@element-plus/icons-vue";
-const props=defineProps({
-    isCollapse:{}
-})
+// -----------------------------emit触发父组件事件-------------------------------------
+const emit = defineEmits(['testFun'])
+// ts的写法  第一种  通过泛型来定义   基于类型的声明   没办法定义默认值
+
+// const props = defineProps<{
+//   isCollapse: boolean
+// }>()
+
+// 第二种   定义单独的接口
+
+interface PropsData {
+  isCollapse: boolean,
+  testProps: object,
+  company?: string
+}
+// const props=defineProps<PropsData>()
+const { isCollapse, testProps, company = '大陆希望' } = defineProps<PropsData>()//结构  可以定义默认值
+// console.log(isCollapse);
+// console.log(testProps);
+// console.log(company);
+
+
+// 常规写法  第三种
+// const props = defineProps({
+//   isCollapse: false
+// })
+
+
 const router = useRouter();
 const menuData = ref([
   // 组件
@@ -73,17 +88,40 @@ const menuData = ref([
     ]
   }
 ]);
+const count = ref(0)//打出来是一个对象  需要.value
+// const count2 = $ref(2)//不需要.value   官网  报错
 // 菜单跳转
 const goMenu = (rowData: any) => {
+  console.log(count)
   router.push(rowData.path);
-};
+  emit('testFun', { 'emit': "子组件传回来的数据" })
+}
+onMounted(() => {
+  console.log('生命周期')
+})
 </script>
+<!-- 不适用setup语法糖的写法 -->
+<!-- <script lang="ts">
+import { defineComponent } from 'vue'
 
+export default defineComponent({
+  props: {
+    isCollapse:Boolean,
+  },
+  setup(props) {
+    console.log(123);
+    
+    console.log(props);
+  }
+})
+
+</script> -->
 <style>
 .el-menu {
   height: 100%;
   border: none;
 }
+
 .el-menu-vertical-demo:not(.el-menu--collapse) {
   width: 200px;
   min-height: 400px;
--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -2,8 +2,8 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <el-aside :width="isCollapse?'64px':'200px'">
-        <Menu :isCollapse="isCollapse"/>
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <Menu :isCollapse="isCollapse" :testProps="testPropsData" :company="company" @test-fun="testFun"/>
       </el-aside>
       <el-container>
         <el-header>
@@ -12,8 +12,8 @@
               <el-image @click="open" style="width: 32px; height: 32px" :src="imgPath" />
             </div>
             <div class="full">
-              <Screenfull >
-               <el-image  style="width: 32px; height: 32px" :src="full" />
+              <Screenfull>
+                <el-image style="width: 32px; height: 32px" :src="full" />
               </Screenfull>
             </div>
           </div>
@@ -30,9 +30,18 @@
 import imgPath from "../assets/header/zhankai.png";
 import full from "../assets/header/full.png";
 let isCollapse = ref(false);
+let company = ref<string>();
+let testPropsData = ref({
+  name: "王健全",
+  age: 27,
+})
 const open = () => {
   isCollapse.value = !isCollapse.value;
 };
+const testFun=(data:object)=>{
+  console.log('子组件触发父组件的方法');
+  console.log(data)
+}
 </script>
 <style scoped>
 .common-layout .el-header,
@@ -41,28 +50,34 @@ const open = () => {
   text-align: center;
   line-height: 60px;
 }
+
 .common-layout .el-footer {
   line-height: 60px;
 }
+
 .el-aside {
   background: #545c64;
 }
+
 .common-layout .el-main {
   background-color: #e9eef3;
   text-align: center;
   padding: 0;
 }
+
 .content {
   height: calc(100vh - 120px);
   padding: 20px;
   box-sizing: border-box;
   overflow: auto;
 }
+
 .header {
   display: flex;
   align-items: center;
   justify-content: space-between;
 }
+
 .header .switch,
 .full {
   cursor: pointer;
